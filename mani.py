da41af0e0578bab3f021cf0e27e2aba12ead099f
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-09-24 15:09:48 -0600

    FIX multi server and rename file

--- mani.py
+++ mani.py
@@ -27,6 +27,7 @@ def Poisson(last, lambdaa):
 		name: name of the server
 """ 
 def server(rate, time, name):
+	# Referencia algoritmo mostrado en la clase
 	T = time
 	t = 0 # time of simulation in seconds
 	Na = 0 # number of arrivals in time t
@@ -86,13 +87,19 @@ def server(rate, time, name):
 	print(f'En promedio, hubieron {abs(dprom - aprom)} solicidted en cola cada segundo')
 	print(f'El momento de salida de la ultima solicitud fue {D[-1]}')
 
+# %%
+server(100/60, 3600, 'Mountain Mega Computers')
+print()
+# %%
 """
 	Simultate multi server
 		time : time of simulation
 		num_servers: number of servers
 		speed: speed of the servers
+		name: name of the servers
 """
-def multi_server(time, num_server, speed):
+def multi(num_servers, time, speed, name):
+	# Referencia algoritmo mostrado en la clase
 	T = time
 	t = 0 # time of simulation in seconds
 	Na = 0 # number of arrivals in time t
@@ -104,72 +111,66 @@ def multi_server(time, num_server, speed):
 	S = [] # list of servers
 
 	ta = exponential(speed)
-	td = [100000]*num_server 
+	td = [100000]*num_servers 
 	tp = 0
 
-	ps = [0]*num_server # process servers
-	ss = [0]*(1 + num_server) # server solution
-
+	ps = [0]*num_servers # process servers
+	ss = [0]*(1 + num_servers) # server solution	
 	while t < T or ss[0] > 0:
 		m = min(td)
 		minimun = td.index(m)
-		if ta > td[minimun] and ta < T: # last arrival before close and still have time
-			t = ta # time of next event
-			Na += 1 # number of arrivals
-			ta = ta + exponential(speed) # time of next arrival
-			A.append(t) # arrival time
-
-			if ss[0] == 0: # if there are no customers in the system
-				S.append(t) # time of arrival to the server
-				ss[0] += 1 # number of customers in the system
-				ss[1] = Na # number of arrival to the server
-				td[0] = t + exponential(speed) # time of next departure
-			elif ss[0] < num_server: # if there are customers in the system but not all servers are busy
-				not_busy = ss.index(0) - 1 # get the first server not busy
-				S.append(t) # time of arrival to the server
-				ss[0] += 1 # number of customers in the system
-				ss[not_busy + 1] = Na # number of arrival to the server
-				td[not_busy] = t + exponential(speed) # time of next departure
-			else: # if all servers are busy
-				ss[0] += 1 # number of customers in the system
+		
+		if ta < td[minimun] and ta< T:   # last arrival before close and still have time
+				t = ta # time of next event
+				Na = Na +  1 # number of arrivals
+				ta = ta + exponential(speed)# time of next arrival
+				A.append(t)# arrival time
+				
+				if ss[0] == 0:# if there are no customers in the system
+						S.append(t)# time of arrival to the server
+						ss[0] += 1# number of customers in the system
+						ss[1] = Na# number of arrival to the server
+						td[0] = t + exponential(speed) # time of next departure
+				elif ss[0] < num_servers: # if there are customers in the system but not all servers are busy
+						notBusy = ss.index(0) - 1 # get the first server not busy
+						S.append(t) # time of arrival to the server
+						ss[0] += 1  # number of customers in the system
+						ss[notBusy+1] = Na # number of arrival to the server
+						td[notBusy] = t + exponential(speed) # time of next departure
+				else: # if all servers are busy
+						ss[0] += 1 # number of customers in the system
 		else: # if the next event is a departure
-			t = td[minimun] # time of next event
-			ps[minimun] += 1 # number of departures
-			D.append(t)	# departure time
+				t = td[minimun]  # time of next event
+				ps[minimun] += 1 # number of departures
+				D.append(t)  # departure time
+				if ss[0] <= num_servers: # if there are still aviailable servers
+						ss[0] -= 1  # number of customers in the system
+						td[minimun] = 100000 # time of next departure
+						ss[minimun + 1] = 0 # remove the customer from the server
+				else: # if there are no available servers
+						ss[0] *= -1 # number of customers in the system
+						siguiente = max(ss)+1 # get the next customer in the queue
+						ss[0] *= -1 # number of customers in the system
+						S.append(t) # time of arrival to the server
+						ss[0] -= 1  # remove the customer from the queue
+						td[minimun] = t + exponential(speed) # time of next departure
+						ss[minimun + 1] = siguiente # number of arrival to the server
 
-			if ss[0] <= num_server: # if there are still aviailable servers
-				ss[0] -= 1 # number of customers in the system
-				td[minimun] = 100000 # time of next departure
-				ss[minimun + 1] = 0 # remove the customer from the server
-			else: # if there are no available servers
-				ss[0] *= -1 # number of customers in the system
-				nextt = max(ss) + 1 # get the next customer in the queue
-				ss[0] *= -1 # number of customers in the system
-				S.apend(t) # time of arrival to the server
-				ss[0] -= 1 # remove the customer from the queue
-				td[minimun] = t + exponential(speed) # time of next departure
-				ss[minimun + 1] = nextt # number of arrival to the server
-	# print results
-	print(f'===== RESULTADO DEL SERVIDOR MULTIPLE =====')
-	print(f'Numero de llegadas: {Na}')
-	for i in range(num_server):
-		print(f'EL servidor {i + 1} tuvo {ps[i]} departures')
-	print(f'El tiempo que estuvo el servidor ocupado fue: {sum(td)}')
-	print(f'El tiempo que estuvo libre el servidor fue de {sum(td) - ta}')
-	print(f'Las solicitudes estuvieron en total {D[-1]}')
-	print(f'En promedio, cada solicitud estuvo en cola {Na/T}')
-	total = 0
-	for i in range(len(A)):
-		diference = S[i] - A[i]
-		total += diference
-	if len(A) > 0:
-		average = total/len(A)
-	else:
-		average = total
-	print(f'En promedio, el tiempo en cola fue de {average}')
-
+	print(f"===== RESULTADOS DEL SERVIDOR MULTIPLE {name} =====")
+	for i in range (len(ps)):
+			print("Servidor ", (i+1), ":", ps[i], "solicitudes")
+	print("Tiempo total ocupado: ", sum(td), "segundos.")
+	print("Solicitudes atendidas: ", Na)
+	tot=0
+	for k in range(len(A)):
+			resta=S[k]-A[k]
+			tot=tot+resta
+	prom = tot/len(A)
+	print('El tiempo total que estuvieron en cola las solicitudes fue de: ', D[-1])
+	print("En promedio cada solicitud estuvo en colo por ", prom, "segundos.")
+	print("En promedio cada solicitud estuvo en cola por ", A[-1]/T, "minutos.")
+	print("Ultima llegada:", A[-1], "s.")
+	print("Ultima salida:", D[-1], "s.")
 # %%
-server(100/60, 3600, 'Mountain Mega Computers')
-print()
-multi_server(3600, 250, 1/6)
+multi(10, 3600, 1/6, "Pizzita computing")
 # %%
